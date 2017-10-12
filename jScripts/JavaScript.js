c47963b5ea7504d96db619035cb9b492f1b3247f
yehodar <yehodar.s@gmail.com>
2017-10-12 16:28:22 +0300

    Add files via upload

---
+++ jScripts/JavaScript.js
@@ -0,0 +1,13 @@
+﻿function open_new() {
+    var url1 = document.getElementById("url_part1").value;
+    var url2 = document.getElementById("url_part2").value;
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    
+    var finelurl;
+    for (i = num1; i <= num2; i++) {
+        finelurl = url1 + i + url2;
+        window.open(finelurl, '_blank');
+        
+    }
+}
